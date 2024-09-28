a9f8356a5d142b45080686e11adeb7f38b691cb9
ramchand32 <mr.ramchand@gmail.com>
2024-09-28 18:10:50 +0530

    Update environment.prod.ts

--- visualization/src/environments/environment.prod.ts
+++ visualization/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  VAAS_BASE_URL: 'https://gstools2pvn.gl.avaya.com:8443/vaas/',
-  // VAAS_BASE_URL: 'https://pwgstvmap01.avaya.com/vaas/',
+  VAAS_BASE_URL: 'https://gstools2pvn.gl.COMP.com:8443/vaas/',
+  // VAAS_BASE_URL: 'https://pwgstvmap01.COMP.com/vaas/',
   GS_SESSION_IDLE_TIMEOUT: 900000, //15 min-15*60*1000,
   GS_GRID_UPDATE_TIMEOUT: 180000,
   SERVICE: 'Visualization'
